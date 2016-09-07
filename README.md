69e5bcbbc35e09808b6faca808841f3eb1d2f962
Josef Ludvicek <jludvice@redhat.com>
2016-09-07 09:49:55 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Memory leak search
+
+Goal of this tiny project is to automate searching for memory leaks in Apache Karaf.  
+It searches for leaked references to classloaders.
+
+## Build
+Build maven project as usual
+```shell
+mvn clean package
+```
+
+
+## Usage
+
+1. take heap dump with command
+    ```shell
+    jmap -dump:format=b,file=file_with_heapdump.hprof PID
+    ```
+
+1. pass dump to this program
+    ```shell
+    java -jar target/memory-leak-search-1.0-SNAPSHOT.jar /path/to/file_with_heapdump.hprof
+    ```
---
+++ src/main/java/org/jboss/fuse/qa/karaf/leak/Main.java
@@ -0,0 +1,105 @@
+package org.jboss.fuse.qa.karaf.leak;
+
+import org.netbeans.lib.profiler.heap.Heap;
+import org.netbeans.lib.profiler.heap.HeapFactory;
+import org.netbeans.lib.profiler.heap.Instance;
+import org.netbeans.lib.profiler.heap.JavaClass;
+import org.netbeans.lib.profiler.heap.PrimitiveArrayInstance;
+import org.netbeans.lib.profiler.heap.Value;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * Created by jludvice on 9/6/16.
+ */
+public class Main {
+    public static final String SYMBOLIC_NAME_FIELD = "m_symbolicName";
+    public static String BUNDLE_REVISION_CLASS_NAME = "org.apache.felix.framework.BundleRevisionImpl";
+    public static String BUNDLE_WIRING_CLASS_NAME = "org.apache.felix.framework.BundleWiringImpl";
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.out.println("pass path to heap dump as argument");
+            System.exit(1);
+        }
+        String fileName = args[0];
+        Path heapDumpPath = Paths.get(fileName).toAbsolutePath();
+
+        if (!Files.exists(heapDumpPath)) {
+            System.out.println("file doesn't exist: " + heapDumpPath);
+            System.exit(1);
+        }
+
+        // load heap dump to netbeans profiler
+        System.out.println("loading file " + heapDumpPath);
+        Heap h = HeapFactory.createHeap(heapDumpPath.toFile());
+
+        // filter all references of given class to instance
+        Predicate<? super Value> wiringFilter = reference -> BUNDLE_WIRING_CLASS_NAME.equals(reference.getDefiningInstance().getJavaClass().getName());
+        // filter instances which have more than 1 reference to itself
+        Predicate<? super Instance> revisionFilter = i -> ((List<Value>) i.getReferences()).stream().filter(wiringFilter).count() > 1;
+
+        // get java class by name
+        JavaClass revision = h.getJavaClassByName(BUNDLE_REVISION_CLASS_NAME);
+
+        // list instances of given class && filter to those with more than 1 reference pointing to it
+        List<Instance> revisionInstances = ((List<Instance>) revision.getInstances()).parallelStream()
+                .filter(revisionFilter)
+                .collect(Collectors.toList());
+
+
+        for (Instance instance : revisionInstances) {
+            String sn = getSymbolicName(instance);
+
+            List<Value> wirings = ((List<Value>) instance.getReferences()).stream()
+                    .filter(wiringFilter)
+                    .sorted((x, y) -> Long.compare(x.getDefiningInstance().getInstanceId(), y.getDefiningInstance().getInstanceId()))
+                    .collect(Collectors.toList());
+
+            // get reference with lowest id
+            Instance wiringInstance = wirings.get(0).getDefiningInstance();
+            // check if there is path to nearest GCRoot
+            Instance root = wiringInstance.getNearestGCRootPointer();
+            if (root == null) {
+                // if no - not interesting, skip this instance
+                continue;
+            }
+            // find out path to root
+            List<Instance> rootPath = new ArrayList<>();
+            while (!root.isGCRoot()) {
+                rootPath.add(root);
+                root = root.getNearestGCRootPointer();
+            }
+
+            // reverse to have same order as in JVisualVM
+            Collections.reverse(rootPath);
+            System.out.println("sn: " + sn);
+            for (int i = 0; i < rootPath.size(); i++) {
+                int n = 1 + i * 2;
+
+                System.out.format("%" + n + "s * (%s) - (%s)\n", "", rootPath.get(i), rootPath.get(i).getJavaClass().getName());
+            }
+        }
+    }
+
+    /**
+     * Get bundle symbolic name from instance of java class from heap dump.
+     *
+     * @param i Instance of java class
+     * @return symbolic name of bundle containing this class
+     */
+    public static String getSymbolicName(Instance i) {
+        Instance symbolicNameInstance = (Instance) i.getValueOfField(SYMBOLIC_NAME_FIELD);
+        PrimitiveArrayInstance snValue = (PrimitiveArrayInstance) symbolicNameInstance.getValueOfField("value");
+        return String.join("", snValue.getValues());
+    }
+}
+
